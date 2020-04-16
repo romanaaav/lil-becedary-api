39e0a4faaa484bdad3aca3ec7c97a84d2fb0db45
Roman Alicando <romanaaav@gmail.com>
2020-04-16 18:37:29 -0300

    All service interfaces created.

--- src/main/java/com/lilbecedary/ws/service/ArtistService.java
+++ src/main/java/com/lilbecedary/ws/service/ArtistService.java
@@ -3,8 +3,11 @@ package com.lilbecedary.ws.service;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.stereotype.Service;
+
 import com.lilbecedary.ws.shared.dto.ArtistDto;
 
+@Service
 public interface ArtistService {
 	List<ArtistDto> getArtists(Map<String, String> queryStrings);
 	ArtistDto getArtist(String publicId);
---
+++ src/main/java/com/lilbecedary/ws/service/impl/ArtistServiceImpl.java
@@ -0,0 +1,23 @@
+package com.lilbecedary.ws.service.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import com.lilbecedary.ws.service.ArtistService;
+import com.lilbecedary.ws.shared.dto.ArtistDto;
+
+public class ArtistServiceImpl implements ArtistService{
+
+	@Override
+	public List<ArtistDto> getArtists(Map<String, String> queryStrings) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArtistDto getArtist(String publicId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
